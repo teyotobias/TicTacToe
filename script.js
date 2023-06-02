0a58bdba3c4879d2758189cfe57f3ed2a0d73940
teyotobias <teyotobias@utexas.edu>
2023-06-01 20:48:57 -0500

    html and css base completed

---
+++ script.js
@@ -0,0 +1,18 @@
+// Minimum Requirements:
+// Display an empty tic-tac-toe board when the 
+// page is initially displayed.
+// A player can click on the nine cells to make a move.
+// Every click will alternate between marking an X and O.
+// Once occupied with an X or O, the cell cannot be played 
+// again.
+// Provide a Reset Game button that will clear the contents
+//  of the board.
+
+
+
+/*----- constants -----*/
+const COLORS = {
+    '0' : 'red',
+    '1' : 'green',
+    '-1' : 'orange'
+};
